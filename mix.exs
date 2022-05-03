fda5dae31745f514a96c153190b269d11efeb977
Metehan Arslan <metehanarslan@gmail.com>
2022-05-03 15:30:24 +0200

    New mix file for hex.pm listing

--- mix.exs
+++ mix.exs
@@ -3,11 +3,15 @@ defmodule Rsa.MixProject do
 
   def project do
     [
-      app: :pk_rsa,
+      app: :rsa,
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      source_url: "https://github.com/metehan/rsa",
+      description: description(),
+      package: package(),
+      deps: deps(),
+      docs: docs()
     ]
   end
 
@@ -21,8 +25,27 @@ defmodule Rsa.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"]
+    ]
+  end
+
+  defp description() do
+    "An ergonomic RSA encrypting decryting library."
+  end
+
+  defp package() do
+    [
+      name: "pk_rsa",
+      files: ~w(lib test mix.exs README.md),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/metehan/rsa"}
     ]
   end
 end
